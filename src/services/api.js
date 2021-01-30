ee22628a98be5f70bf29d725eeaa14bca32df263
Bruna Santana <brunamarjorie@gmail.com>
2021-01-30 23:33:26 +0000

    comments

--- src/services/api.js
+++ src/services/api.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 //fetch API
 const api = axios.create({
     //local host
-    baseURL: 'http://localhost:3001'
+    //baseURL: 'http://localhost:3001'
     //heroku api
-    //baseURL: 'https://santanas-api.herokuapp.com'
+    baseURL: 'https://santanas-api.herokuapp.com'
 });
 
 
